7eb251b082ed5a384e2b3ae196678540065e0204
Mojtaba Razavi <63773350+razavi1993@users.noreply.github.com>
2021-11-14 19:22:36 +0330

    Add files via upload

--- scene-rl-based/surfaces.jl
+++ scene-rl-based/surfaces.jl
@@ -7,10 +7,6 @@ struct xy_rect <: hitable
     y2::Float64
     normal::vec
     mater::material
-    m::Int64
-    n::Int64
-    qtable::Dict{Int64,Array{Float64,2}}
-    paths::Dict{Int64,Array{Int64,2}}
 end
 
 # yz rectangle
@@ -22,10 +18,6 @@ struct yz_rect <: hitable
     z2::Float64
     normal::vec
     mater::material
-    m::Int64
-    n::Int64
-    qtable::Dict{Int64,Array{Float64,2}}
-    paths::Dict{Int64,Array{Int64,2}}
 end
 
 # xz rectangle
@@ -37,10 +29,6 @@ struct xz_rect <: hitable
     z2::Float64
     normal::vec
     mater::material
-    m::Int64
-    n::Int64
-    qtable::Dict{Int64,Array{Float64,2}}
-    paths::Dict{Int64,Array{Int64,2}}
 end
 
 # sphere
@@ -48,10 +36,6 @@ struct sphere <: hitable
     radius::Float64
     center::vec
     mater::material
-    m::Int64
-    n::Int64
-    qtable::Dict{Int64,Array{Float64,2}}
-    paths::Dict{Int64,Array{Int64,2}}
 end
 
 # hitable list
@@ -69,10 +53,7 @@ function hit(rect::xy_rect, r::ray, tmin::Float64, tmax::Float64)
     if (p.x < rect.x1 || p.x > rect.x2 || p.y < rect.y1 || p.y > rect.y2)
         return missing
     end
-    i = ceil(Int64, rect.m*(p.x - rect.x1)/(rect.x2 - rect.x1))
-    j = ceil(Int64, rect.n*(p.y - rect.y1)/(rect.y2 - rect.y1))
-    cell_id = rect.n*(i-1) + j
-    return record(t, p, rect.normal, rect.mater, cell_id, rect.qtable, rect.paths)
+    return record(t, p, rect.normal, rect.mater)
 end
 
 # xz rectangle hit function
@@ -85,10 +66,7 @@ function hit(rect::xz_rect, r::ray, tmin::Float64, tmax::Float64)
     if (p.x < rect.x1 || p.x > rect.x2 || p.z < rect.z1 || p.z > rect.z2)
         return missing
     end
-    i = ceil(Int64, rect.m*(p.x - rect.x1)/(rect.x2 - rect.x1))
-    j = ceil(Int64, rect.n*(p.z - rect.z1)/(rect.z2 - rect.z1))
-    cell_id = rect.n*(i-1) + j
-    return record(t, p, rect.normal, rect.mater, cell_id, rect.qtable, rect.paths)
+    return record(t, p, rect.normal, rect.mater)
 end
 
 # yz rectangle hit function
@@ -101,10 +79,7 @@ function hit(rect::yz_rect, r::ray, tmin::Float64, tmax::Float64)
     if (p.y < rect.y1 || p.y > rect.y2 || p.z < rect.z1 || p.z > rect.z2)
         return missing
     end
-    i = ceil(Int64, rect.m*(p.y - rect.y1)/(rect.y2 - rect.y1))
-    j = ceil(Int64, rect.n*(p.z - rect.z1)/(rect.z2 - rect.z1))
-    cell_id = rect.n*(i-1) + j
-    return record(t, p, rect.normal, rect.mater, cell_id, rect.qtable, rect.paths)
+    return record(t, p, rect.normal, rect.mater)
 end
 
 # sphere hit function
@@ -113,33 +88,20 @@ function hit(s::sphere, r::ray, tmin::Float64, tmax::Float64)
     b = 2.0*dot(r.d, r.o - s.center)
     c = dot(r.o - s.center, r.o - s.center) - s.radius*s.radius
     Δ = b*b - 4.0*a*c
-    if Δ <= 0.
+    if Δ < 0.
         return missing
-    end
-    t1 = (-b-sqrt(Δ))/(2.0*a)
-    t2 = (-b-sqrt(Δ))/(2.0*a)
-    if t2 > 0.
-        if t1 > 0.
-            p = point(r, t1)
-            normal = unit(p - s.center)
-            u = acos(normal.y)/π
-            v = (π + atan(normal.x, normal.z))/(2.0*π)
-            i = ceil(Int64, s.m*u)
-            j = ceil(Int64, s.n*v)
-            cell_id = s.n*(i-1) + j
-            return record(t1, p, normal, s.mater, cell_id, s.qtable, s.paths)
+    else
+        t1 = (-b-sqrt(Δ))/(2.0*a)
+        t2 = (-b+sqrt(Δ))/(2.0*a)
+        if t2 < 0. 
+            return missing
         else
-            p = point(r, t2)
-            normal = unit(s.center - p)
-            u = acos(-1.0*normal.y)/π
-            v = (π + atan(-1.0*normal.x, -1.0*normal.z))/(2.0*π)
-            i = ceil(Int64, s.m*u)
-            j = ceil(Int64, s.n*v)
-            cell_id = s.n*(i-1) + j
-            return record(t2, p, normal, s.mater, cell_id, s.qtable, s.paths)
+            t = (t1 > 0.) ? t1 : t2
+            p = point(r, t)
+            normal = unit(p - s.center)
+            return record(t, p, normal, s.mater)
         end
     end
-    return missing
 end
 
 # nearest object
@@ -148,7 +110,7 @@ function hit(h::hit_list, r::ray, tmin::Float64, tmax::Float64)
     hit_surface = missing
     for surface in h.list
         temp_surface = hit(surface, r, tmin, tc)
-        if !ismissing(temp_surface)
+        if !ismissing(temp_surface) && tmin < temp_surface.t < tc
             hit_surface = temp_surface
             tc = hit_surface.t
         end
